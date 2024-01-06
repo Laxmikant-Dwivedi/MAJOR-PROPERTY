1e3897c79be75cce7e45d84f41c0129586903a85
Laxmikant-Dwivedi <dwivedilakshya7509@gmail.com>
2024-01-06 22:11:24 +0530

    Initial Commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className='text-3xl font-bold mb-4 text-slate-800'>About Homeizz</h1>
+      <p className='mb-4 text-slate-700'>Homeizz is a leading real estate agency that specializes in helping clients buy, sell, and rent properties in the most desirable neighborhoods. Our team of experienced agents is dedicated to providing exceptional service and making the buying and selling process as smooth as possible.</p>
+      <p className='mb-4 text-slate-700'>
+      Our mission is to help our clients achieve their real estate goals by providing expert advice, personalized service, and a deep understanding of the local market. Whether you are looking to buy, sell, or rent a property, we are here to help you every step of the way.
+      </p>
+      <p className='mb-4 text-slate-700'>Homeizz is your number one free real estate listing website template. It is a modern, professional and sophisticated tool that will speed up the process of building a top-notch web space. To your luck, with Homeizz, you do not need to kick things off from the ground up anymore. If you fancy the out of the box look, by all means, utilize this site canvas precisely as is. Of course, you can also alter and brand the look according to your real estate firm’s needs and regulations.</p>
+    </div>
+  )
+}
